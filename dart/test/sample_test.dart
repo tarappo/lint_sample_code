5c5826c1fb4a91a081e42ada4aa4f2a7233f71d9
tarappo <tarappo@gmail.com>
2025-01-13 06:16:49 +0900

    サンプルのテストケースを追加

---
+++ dart/test/sample_test.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('サンプルグループ', () {
+    test("サンプルテストケース", () async {
+    });
+    test('日本語てすとけーす', () async {
+    });
+    test('only English Test Case Name', () async {
+    });
+  });
+
+  group('only English Group Name', () {
+    test("サンプルテストケース", () async {
+    });
+    test('日本語てすとけーす', () async {
+    });
+    test('only English Test Case Name', () async {
+    });
+  });
+}
