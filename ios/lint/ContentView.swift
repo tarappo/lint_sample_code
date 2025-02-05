a0b8e5db4d1c0e1ea3000bfbe9847129f75586a1
tarappo <tarappo@gmail.com>
2025-02-06 05:41:11 +0900

    iOSのサンプルコードを追加

---
+++ ios/lint/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lint
+//
+//  Created by Toshiyuki Hirata on 2025/02/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/lint/lintApp.swift
@@ -0,0 +1,19 @@
+//
+//  lintApp.swift
+//  lint
+//
+//  Created by Toshiyuki Hirata on 2025/02/02.
+//
+
+import SwiftUI
+import SwiftSyntax
+import SwiftParser
+
+@main
+struct lintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
